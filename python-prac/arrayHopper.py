a95c5ddcce0fdfba6f18ac9abc02b1d08ed90947
bhuang92 <basilhuang2014@u.northwestern.edu>
2013-11-14 17:09:37 -0600

    removed cached items. finished array hopper

--- python-prac/arrayHopper.py
+++ python-prac/arrayHopper.py
@@ -1,54 +1,62 @@
 #!/usr/bin/env python
-
 import sys, copy
 
-#Set current shortest solution length to INF for pruning
-shortestSolutionLength = 9999999
 allPossibleSolutions = []
+#keep track of current shortest solution length
+shortestSolutionLength = 999999
 
 #Parse the input file and store it into an array
 def initialize():
+	'''
+	1. Read in the input and store it into an array
+	2. Pass in the array, the solution array so far, and the input length to the recursive function
+	'''
 	inputArray = [int(line.strip()) for line in open(sys.argv[1], 'r')]
-	print inputArray
-	arrayLength = len(inputArray)
+	arrayLength = len(inputArray)-1
 
-	#solutionArray will contain pairs of (index, distanceTraveledSoFar)
+	#solutionArray will contain a list of indexes
 	solutionArray = []
-	solutionArray.append((0,0))
-	print solutionArray
-	print arrayLength
-	forward_recur(inputArray, solutionArray, arrayLength-inputArray[0])
+	solutionArray.append(0)
+	forward_recur(inputArray, solutionArray, arrayLength)
 
 def forward_recur(inputArray, solutionArray, lengthLeft):
-	currIndex = solutionArray[-1]
-	indexRange = inputArray[currIndex[0]]
-
-	print "(index, distance traveled so far)"
-	print currIndex
-	print "this range"
-	print indexRange
+	'''
+	Input: the input array, the current solution array so far, the remaining length
+	This function will recursively check each hop possibility to check if we can fully traverse the array.
+	Optimizations: this algorithm keeps track of the current shortest path. If a possible solution branch
+	is as long as the shortest solution, then ignore it.
+	'''
 	#Check if we are within range of exiting the array
-	if(lengthLeft<=0):
-		#We have a solution, so add it to the array
-		allPossibleSolutions.append(solutionArray)
+	global shortestSolutionLength
+	if(len(solutionArray)>=shortestSolutionLength):
 		return
 	else:
-		#We didn't have a solution, so recurse
-		lengthLeft -= inputArray[i]
-		for i in range(currIndex[1], currIndex[1]+indexRange):
-			print "Going through this index now..."
-			print i
-			solutionArrayCopy = copy.deepcopy(solutionArray)
-			solutionArrayCopy.append((i, inputArray[i]+currIndex[1]))
-			print solutionArrayCopy
-			forward_recur(inputArray, solutionArrayCopy, lengthLeft)
-	return
+		if(lengthLeft<=0):
+			if(shortestSolutionLength > len(solutionArray)):
+				#We are able to hop out of the array with this node, so add this solution to the array
+				del solutionArray[-1] #get rid of last hop destination
+				allPossibleSolutions.append(solutionArray)
+				shortestSolutionLength = len(solutionArray)
+			return
+		else:
+			currIndex = solutionArray[-1]
+			indexRange = inputArray[currIndex]
+			# We didn't have a solution, so recurse through every option
+			for i in range(currIndex+1, 1+currIndex+indexRange):
+				solutionArrayCopy = copy.deepcopy(solutionArray)
+				solutionArrayCopy.append(i)
+				forward_recur(inputArray, solutionArrayCopy, len(inputArray)-i)
+		return
 
+#Run the functions
 initialize()
+if(len(allPossibleSolutions)==0): # no solutions, so print failure
+	print "failure"
+else: # we have solutions
+	output = sorted(allPossibleSolutions, key=lambda L: len(L))[0] # return one with shortest # of hops
+	outputStr = ""
+	for num in output:
+		outputStr += str(num) + ", "
+	outputStr+="out"
+	print outputStr
 
-for item in allPossibleSolutions:
-	print item
-
-
-#Return element with fewest number of elements
-#sorted(myList, key=lambda L: len(L))[0]
